cfc3edaeb0627a8ea2d11ad269ca4a9e785c81f7
Oli Sturm <oliver@oliversturm.com>
2022-06-28 15:12:51 +0100

    initial

--- sveltekit-demo/final/src/routes/[one]/[two]/thing/[...name].svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-	const { one, two, name } = $page.params;
-</script>
-
-<div>Arrived at test page, one='{one}', two='{two}', name is '{name}'</div>
--- sveltekit-demo/final/src/routes/[start].simple.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-</script>
-
-<div>Arrived at simple page, start='{$page.params.start}'</div>
